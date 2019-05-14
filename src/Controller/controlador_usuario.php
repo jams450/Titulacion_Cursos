eec126b498e4f6952a02daa0352a6d17cd8b5ea9
jams450 <jams45072@gmail.com>
2019-05-14 11:18:40 -0500

    creacion usuario

--- src/Controller/controlador_usuario.php
+++ src/Controller/controlador_usuario.php
@@ -1,20 +1,23 @@
 <?php
 
     include_once($_SERVER["DOCUMENT_ROOT"] . "/src/Model/conexion.php");
-    //var_dump($_POST);
-    $stmt=$conexion->prepare("insert into usuarios (nombre,appat,apmat,edad,sexo,correo,username,passwd,idtipopago) values (?,?,?,?,?,?,?,?,?)");
-    echo $stmt->error;
-    $stmt->bind_param("sssiisssi", $_POST['nombre'], $_POST['appat'], $_POST['apmat'], $_POST['edad'], $_POST['sexo'], $_POST['correo'], $_POST['username'], $_POST['passwd'], $_POST['tipopago']);
-    $stmt->execute();
 
-    if ($stmt->affected_rows) {
-        $msj="Exito";
-    //die(json_encode($msj)); //codificar en json, el die es como un return, se puede sustituir el die directamente con un return
+    $resultado=$conexion->query("select correo from usuarios where correo = '".$_POST['correo']."'");
+    if ($res = $resultado->fetch_assoc()) {
+        $msj="Ya existe correo";
     } else {
-        $msj="Mal";
-        //die(json_encode($stmt->error));
-    }
+        $stmt=$conexion->prepare("insert into usuarios (nombre,appat,apmat,edad,sexo,correo,username,passwd,idtipopago) values (?,?,?,?,?,?,?,?,?)");
+        echo $stmt->error;
+        $stmt->bind_param("sssiisssi", $_POST['nombre'], $_POST['appat'], $_POST['apmat'], $_POST['edad'], $_POST['sexo'], $_POST['correo'], $_POST['username'], $_POST['passwd'], $_POST['tipopago']);
+        $stmt->execute();
 
+        if ($stmt->affected_rows) {
+            $msj="Exito";
+        } else {
+            $msj="Mal";
+        }
+        $stmt->close();
+    }
     echo $msj;
-    $stmt->close();
+
     $conexion->close();
