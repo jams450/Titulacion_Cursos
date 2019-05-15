b21da65a7aa7ab71ee8dcc764a37e6d086270b9f
jams450 <jams45072@gmail.com>
2019-05-14 19:15:14 -0500

    registro terminado -> faltana mensajes de alerta

--- src/Controller/controlador_usuario.php
+++ src/Controller/controlador_usuario.php
@@ -7,52 +7,21 @@
     $comprobacion_usuario=$conexion->query("select username from usuarios where username='".$_POST['username']."'");
 
     if ($result=$comprobacion_correo->fetch_assoc()) {
-      ?>
-        <script type="text/javascript" src="/src/js/alta_usuario.js"></script>
-      <?php
-    }else if ($res=$comprobacion_usuario->fetch_assoc()){
-      echo'<script type="text/javascript">alert("NOMBRE DE USUARIO REGISTRADO PREVIAMENTE.");</script>';
-    }else {
-      $stmt=$conexion->prepare("insert into usuarios (nombre,appat,apmat,edad,sexo,correo,username,passwd,idtipopago) values (?,?,?,?,?,?,?,?,?)");
-      echo $stmt->error;
-      $stmt->bind_param("sssiisssi", $_POST['nombre'], $_POST['appat'], $_POST['apmat'], $_POST['edad'], $_POST['sexo'], $_POST['correo'], $_POST['username'], $_POST['passwd'], $_POST['tipopago']);
-      $stmt->execute();
-
-      if ($stmt->affected_rows) {
-          //$msj="Exito";
-          echo'<script type="text/javascript">alert("NUEVO USUARIO REGISTRADO. /n VERIFICA TU CUENTA DESDE TU CORREO.");</script>';
-          //die(json_encode($msj)); //codificar en json, el die es como un return, se puede sustituir el die directamente con un return
-      } else {
-          $msj="Mal";
-          //die(json_encode($stmt->error));
-      }
-      echo $msj;
-
-      $stmt->close();
-    }
-
-    $conexion->close();
-      /*if ($res=$comprobacion_usuario->fetch_assoc()) {
-        echo'<script type="text/javascript">alert("NOMBRE DE USUARIO REGISTRADO PREVIAMENTE.");</script>';
-      }else {
+        $msj="correo_mal";
+    } elseif ($res=$comprobacion_usuario->fetch_assoc()) {
+        $msj="usuario_mal";
+    } else {
         $stmt=$conexion->prepare("insert into usuarios (nombre,appat,apmat,edad,sexo,correo,username,passwd,idtipopago) values (?,?,?,?,?,?,?,?,?)");
         echo $stmt->error;
         $stmt->bind_param("sssiisssi", $_POST['nombre'], $_POST['appat'], $_POST['apmat'], $_POST['edad'], $_POST['sexo'], $_POST['correo'], $_POST['username'], $_POST['passwd'], $_POST['tipopago']);
         $stmt->execute();
 
         if ($stmt->affected_rows) {
-            $msj="Exito";
-            echo'<script type="text/javascript">alert("NUEVO USUARIO REGISTRADO. VERIFICA TU CUENTA DESDE TU CORREO.");</script>';
-            //die(json_encode($msj)); //codificar en json, el die es como un return, se puede sustituir el die directamente con un return
+            $msj="exito";
         } else {
-            $msj="Mal";
-            //die(json_encode($stmt->error));
+            $msj="error";
         }
-        echo $msj;
-
         $stmt->close();
-      }
-      $conexion->close();
-    }*/
-    //window.location.href="/view/registro_usuario.php"
-?>
+    }
+    $conexion->close();
+    die(json_encode($msj));
--- src/js/alta_usuario.js
+++ src/js/alta_usuario.js
@@ -1,44 +1,56 @@
-
 $(document).ready(function() {
-
-
   var valid = $("#newuser").validate({
-      errorElement: "label",
-      errorClass: 'error_form',
-      errorPlacement: function(label, element) {
-        var div = $(element).closest(".form_re");
-        if (element.attr("class") == "styled-checkbox check_act") {
-          if (val) {
-            label.insertAfter($('#turisticas'));
-            val = false;
-          }
-        } else
-          label.insertAfter(div);
+    errorElement: "label",
+    errorClass: 'error_form',
+    errorPlacement: function(label, element) {
+      var div = $(element).closest(".form_re");
+      if (element.attr("class") == "styled-checkbox check_act") {
+        if (val) {
+          label.insertAfter($('#turisticas'));
+          val = false;
+        }
+      } else
+        label.insertAfter(div);
 
-      },
-      highlight: function(element) {
-        $(element).closest('.form_re').addClass('has-error');
-      },
-      unhighlight: function(element) {
-        $(element).closest('.form_re').removeClass('has-error');
-      }
+    },
+    highlight: function(element) {
+      $(element).closest('.form_re').addClass('has-error');
+    },
+    unhighlight: function(element) {
+      $(element).closest('.form_re').removeClass('has-error');
+    }
   });
+});
 
 
-    var datos=$('#newuser').serializeArray();
-      $.ajax({
-        // En data puedes utilizar un objeto JSON, un array o un query string
+$('#registro_usuario').click(function(event) {
+  event.preventDefault();
+  if ($('#newuser').valid()) {
+    var datos = $('#newuser').serializeArray();
+    $.ajax({
         data: datos,
-        //Cambiar a type: POST si necesario
         type: "POST",
-        // Formato de datos que se espera en la respuesta
         dataType: "json",
-        // URL a la que se enviará la solicitud Ajax
-        url: "controlador_usuario.php",
-    })
-     .done(function(res) {
-          $('#resultado').html(res);
-          alert("CORREO REGISTRADO PREVIAMENTE");
-     })
+        url: "/src/Controller/controlador_usuario.php",
+      })
+      .done(function(res) {
+        var data = res;
+        switch (data) {
+          case 'exito':
+            alert(data);
+            break;
+          case 'correo_mal':
+            alert(data);
+            break;
+          case 'usuario_mal':
+            alert(data);
+            break;
+          case 'error':
+            alert(data);
+            break;
+          default:
 
+        }
+      })
+  }
 });
